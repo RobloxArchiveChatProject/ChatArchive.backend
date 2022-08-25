6759c2f42da286f1b521b5191c4877f2b571033e
Algasami <105358814+algasami@users.noreply.github.com>
2022-08-25 16:23:12 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const { name, version, author, license } = require("../package.json");
+import express from "express";
+import router from "./routers/archive.js";
+
+var app = express();
+
+app.get("/", function (req, res) {
+	res.send(`${name}: Build Version ${version} | Released Under ${license}`);
+});
+
+app.use("/archive", router);
+
+app.listen(3000);
---
+++ src/routers/archive.js
@@ -0,0 +1,57 @@
+import bodyParser from "body-parser";
+import { randomUUID } from "crypto";
+import { Router } from "express";
+import fs, { rmSync } from "fs";
+var router = Router();
+
+router.get("/", function (req, res) {
+	res.send("GET route on things.");
+});
+
+var json_parser = bodyParser.json();
+router.post("/createGroup", json_parser, function (req, res) {
+	if (req.body == undefined) {
+		res.send("{}");
+		return;
+	}
+	let uuid = randomUUID();
+	res.send(
+		JSON.stringify({
+			uuid: uuid,
+		})
+	);
+	let list = JSON.parse(
+		fs.readFileSync("./src/data/filelist.json", {
+			encoding: "utf-8",
+			flag: "r",
+		})
+	);
+	list.push(uuid + ".json");
+	fs.writeFileSync("./src/data/filelist.json", JSON.stringify(list), {
+		encoding: "utf-8",
+		flag: "w",
+	});
+	fs.writeFileSync("./src/data/" + uuid + ".json", JSON.stringify(req.body), {
+		encoding: "utf-8",
+		flag: "w",
+	});
+});
+router.post("/addStream", json_parser, function (req, res) {
+	let id = req.body.uuid;
+	let doc = JSON.parse(
+		fs.readFileSync("./src/data/" + id + ".json", { encoding: "utf-8" })
+	);
+	doc.data.push({ user: req.body.user, message: req.body.message });
+	fs.writeFileSync("./src/data/" + id + ".json", JSON.stringify(doc), {
+		encoding: "utf-8",
+		flag: "w",
+	});
+	res.send(
+		JSON.stringify({
+			Success: true,
+		})
+	);
+});
+
+//export this router to use in our index.js
+export default router;
